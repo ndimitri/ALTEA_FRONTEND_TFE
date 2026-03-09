993317b58da3dc98b1387b1ace910307d861bff7
Nicolas Dimitriadis <dimitriadisnicolas@gmail.com>
2026-03-09 22:45:49 +0100

    feat: enhance PWA support with service worker and manifest updates

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { routes } from './app.routes';
-import { AuthInterceptor } from './core/guards/auth.guard';
+import {ApplicationConfig, isDevMode} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {routes} from './app.routes';
+import {AuthInterceptor} from './core/guards/auth.guard';
+import {provideServiceWorker} from "@angular/service-worker";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +15,10 @@ export const appConfig: ApplicationConfig = {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
       multi: true
-    }
+    },
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };
